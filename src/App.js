260c4dfa8a8d8bde859d6990a297f6ce402cf4b2
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-28 13:24:21 -0700

    auth is working and redirecting user to items page if they are signed in

--- src/App.js
+++ src/App.js
@@ -10,13 +10,21 @@ import backgroundImage from './supermarket.jpeg';
 function App() {
   const { user } = useContext(UserContext);
 
+
+
   return (
     <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <Header />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
         <Route path="/items" component={Items} />
-        <Route exact path="/"></Route>
+        <Route exact path="/">
+      
+          {!user && <Redirect to="/auth/sign-in" />}
+          {user && <Redirect to="/items" />}
+        
+
+        </Route>
       </Switch>
     </div>
   );
