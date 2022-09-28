77c3d8c347bb3adb24fee246f66b39ce4d95f81e
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-28 11:51:38 -0700

    wrapped app in UserProvider context

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bulma/css/bulma.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './context/userContext';
 
 // TODO -- make sure to wrap your app in your User provider!
 
@@ -12,8 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
+    
   </React.StrictMode>
 );
 
